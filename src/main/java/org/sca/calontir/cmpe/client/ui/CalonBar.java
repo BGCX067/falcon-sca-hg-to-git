ff7d010f5831d5a7b29ff311a007e88c3bb14609
rikscarborough <rikscarborough@Riks-iMac.local>
2012-02-27 19:11:08 -0600

    CalonBar fixes.

--- src/main/java/org/sca/calontir/cmpe/client/ui/CalonBar.java
+++ src/main/java/org/sca/calontir/cmpe/client/ui/CalonBar.java
@@ -5,6 +5,7 @@
 package org.sca.calontir.cmpe.client.ui;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.*;
 import org.sca.calontir.cmpe.client.LoginInfo;
@@ -17,24 +18,33 @@ import org.sca.calontir.cmpe.client.LoginServiceAsync;
  */
 public class CalonBar extends Composite {
 
+    protected static final String CALONBAR = "calonbar";
+    protected static final String INDEXHTML = "/index.html";
+    protected static final String ABOUT_PAGE = "/about.jsp";
+    protected static final String CALONBARLINK = "calonbarlink";
+    protected static final String SIGN_IN_TEXT = "Please sign in to your Google Account.";
+    protected static final String SIGN_OUT_TEXT = "This will log you out of your Google Account.";
     private Panel barPanel = new FlowPanel();
+    private Panel loginPanel = new FlowPanel();
     private LoginInfo loginInfo = null;
     private Anchor homeLink = new Anchor("Home");
+    private Anchor aboutLink = new Anchor("About");
     private Anchor signInLink = new Anchor("Sign In");
     private Anchor signOutLink = new Anchor("Sign Out");
-    private Label loginLabel = new Label("Please sign in to your Google Account to access the StockWatcher application.");
 
     public CalonBar() {
 
-        barPanel.setStylePrimaryName("calonbar");
+//        barPanel.setStyleName(CALONBAR);
+        DOM.setElementAttribute(barPanel.getElement(), "id", CALONBAR);
 
-        homeLink.setHref("/index.html");
-        homeLink.setStylePrimaryName("calonbarlink");
+        homeLink.setHref(INDEXHTML);
+        homeLink.setStyleName(CALONBARLINK);
         barPanel.add(homeLink);
 
-        Label k = new Label();
-        k.setText("|");
-        barPanel.add(k);
+        barPanel.add(getDivBar());
+        
+        loginPanel.setStyleName(CALONBARLINK);
+        barPanel.add(loginPanel);
 
         LoginServiceAsync loginService = GWT.create(LoginService.class);
         loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<LoginInfo>() {
@@ -45,28 +55,45 @@ public class CalonBar extends Composite {
             public void onSuccess(LoginInfo result) {
                 loginInfo = result;
                 if (loginInfo.isLoggedIn()) {
-                    signOutLink.setHref(loginInfo.getLogoutUrl());
-                    signOutLink.setStylePrimaryName("calonbarlink");
-                    if (loginInfo.getScaName() == null) {
-                        signOutLink.setText(loginInfo.getNickname());
-                    } else {
-                        signOutLink.setText(loginInfo.getScaName());
-                    }
-                    barPanel.add(signOutLink);
+                    loadLogout();
                 } else {
                     loadLogin();
                 }
             }
         });
 
+        barPanel.add(getDivBar());
+
+        aboutLink.setHref(ABOUT_PAGE);
+        aboutLink.setStyleName(CALONBARLINK);
+        barPanel.add(aboutLink);
+
         initWidget(barPanel);
     }
 
+    private Label getDivBar() {
+        Label divBar = new Label();
+        divBar.setText("|");
+        divBar.setStyleName(CALONBARLINK);
+        return divBar;
+    }
+
     private void loadLogin() {
-        // Assemble login panel.
         signInLink.setHref(loginInfo.getLoginUrl());
-        signInLink.setStylePrimaryName("calonbarlink");
-//        loginPanel.add(loginLabel);
-        barPanel.add(signInLink);
+        signInLink.setStyleName(CALONBARLINK);
+        signInLink.setTitle(SIGN_IN_TEXT);
+        loginPanel.add(signInLink);
+    }
+
+    private void loadLogout() {
+        signOutLink.setHref(loginInfo.getLogoutUrl());
+        signOutLink.setStyleName(CALONBARLINK);
+        signOutLink.setTitle(SIGN_OUT_TEXT);
+        if (loginInfo.getScaName() == null) {
+            signOutLink.setText(loginInfo.getNickname());
+        } else {
+            signOutLink.setText(loginInfo.getScaName());
+        }
+        loginPanel.add(signOutLink);
     }
 }
